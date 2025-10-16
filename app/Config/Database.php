d7da5ea041ad40c16bed7cb8dde2a123971a89cd
Jorge Velarde <jorge.velarde.r@gmail.com>
2025-10-16 02:39:19 -0300

    feat(auth): se agregar codingiter shield para auth

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -199,5 +199,10 @@ class Database extends Config
         if (ENVIRONMENT === 'testing') {
             $this->defaultGroup = 'tests';
         }
+        
+        $this->default['hostname'] = env('database.default.hostname');
+        $this->default['username'] = env('database.default.username');
+        $this->default['password'] = env('database.default.password');
+        $this->default['database'] = env('database.default.database');
     }
 }
